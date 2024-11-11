5c9fdc8ae7da6e4fe65895b2125be4593b8a6b48
karakure178 <karakure1780@gmail.com>
2024-11-11 20:44:29 +0900

    fix - kvのレイアウト修正

--- src/components/top/info/InfoSection.tsx
+++ src/components/top/info/InfoSection.tsx
@@ -1,15 +1,16 @@
 import { useAPI } from "./../../context/dataContext";
+import "./InfoSection.scss";
 
 const InfoSection = () => {
   const { users } = useAPI();
   console.log(users);
   return (
-    <section>
-      <div className="flex justify-center p-5">
+    <section className="infoSection">
+      <div className="infoSection__inner">
         {users.length === 0 ? (
           <p className="">~読み込み中~</p>
         ) : (
-          <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900 text-center">
+          <h1 className="infoSection__headline title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900 text-center">
             {users.text}
           </h1>
         )}
